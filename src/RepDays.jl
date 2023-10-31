755b3be458611b0e0e47f281d2c8e227dca7c8c8
Peter Volkmar <peter.volkmar@energy2020.com>
2023-10-31 09:42:39 -0400

    Get Day cycle working

--- src/RepDays.jl
+++ src/RepDays.jl
@@ -2,4 +2,16 @@ module RepDays
 
 greet() = print("Hello World!")
 
+function generate_ldc(hours::Int, days::Int)
+  hotday = round(days / 2)
+  hothour = round(hours * 5 / 6)
+  area_mean = rand()
+  day = zeros(days)
+  day[1] = 2 * area_mean / 3 + (cos(abs(1 - hotday) / hotday)) / 2 + rand()
+  for i in 2:days
+    day[i] = (day[i-1] + area_mean + cos(abs(i - hotday) / hotday)) / 3 # + rand()
+  end
+  return (day)
+end
+
 end # module RepDays
